7957f3765923e025ddd0132f52bccdd03c775842
Krzysztof Grabarczyk <s16459@pjwstk.edu.pl>
2020-04-09 00:50:50 +0200

    Ćwiczenia czwarte pierwsze podejści

---
+++ Cw4/Cw4/Controllers/StudentsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw4.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private string SqlConn = "Data Source=db-mssql;Initial Catalog=s16459;Integrated Security=True";
+
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            var output = new List<Student>();
+            using (var client = new SqlConnection(SqlConn))
+            {
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = client;
+                    command.CommandText = "Select * from Students";
+
+                    client.Open();
+                    var dr = command.ExecuteReader();
+
+                    while (dr.Read())
+                    {
+                        output.Add(new Student
+                        {
+                            IndexNumber = int.Parse(dr["IndexNumber"].ToString()),
+                            FirstName = dr["FirstName"].ToString(),
+                            LastName = dr["LastName"].ToString(),
+                            BirthDate = dr["BirthDate"].ToString(),
+                            IdEnrollment = int.Parse(dr["IdEnrollment"].ToString())
+                        });
+                    }
+                }           
+            }
+        }
+    }
+}
